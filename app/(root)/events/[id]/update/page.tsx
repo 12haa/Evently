ac7d5a770f07c2f377be156cc3854b64abca6d6e
12haa <12hadoto@gmail.com>
2023-12-17 11:15:33 +0330

    Add shadcn-form

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -8,6 +8,7 @@ const UpdateEvent = () => {
   const userId = sessionClaims?.userId as string;
   return (
     <>
+      f
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h3-bold text-center sm:text-left">
           {" "}
