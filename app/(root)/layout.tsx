c35f8d0495b5ff9d9ee2586da7c0d612cba28b79
12haa <12hadoto@gmail.com>
2023-12-17 09:20:24 +0330

    Add EventForm (Create - Update) Routes / Components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,15 +3,15 @@ import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <div className="flex h-screen flex-col" >
-        <Header/>
-        <main className="flex-1 ">{children}</main>
-        <Footer />
-        </div>
-    )
+  return (
+    <div className="flex h-screen flex-col ">
+      <Header />
+      <main className="flex-1 ">{children}</main>
+      <Footer />
+    </div>
+  );
 }
