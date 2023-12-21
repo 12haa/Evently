0165d29202dad2d11bbb69a221fb0e18ccdff6ea
12haa <12hadoto@gmail.com>
2023-12-21 12:25:18 +0330

    Finish UpdateEvent In EventForm

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import EventForm from "@/components/shared/EventForm";
 import { auth } from "@clerk/nextjs";
-
-const UpdateEvent = () => {
+import { getEventById } from "@/lib/actions/event.actions";
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
-
   const userId = sessionClaims?.userId as string;
+  const event = await getEventById(id);
+
   return (
     <>
-      f
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h3-bold text-center sm:text-left">
           {" "}
@@ -16,7 +21,12 @@ const UpdateEvent = () => {
         </h3>
       </section>
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          userId={userId}
+          type="Update"
+          event={event}
+          eventId={event._id}
+        />
       </div>
     </>
   );
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -29,15 +29,26 @@ import { useRouter } from "next/navigation";
 // import { createEvent, updateEvent } from "@/lib/actions/event.actions"
 // import { IEvent } from "@/lib/database/models/event.model"
 import { useUploadThing } from "@/lib/uploadthing";
-import { createEvent } from "@/lib/actions/event.actions";
+import { createEvent, updateEvent } from "@/lib/actions/event.actions";
+import { IEvent } from "@/lib/mongodb/database/models/event.model";
 
 //
 type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
+  event?: IEvent;
+  eventId?: string;
 };
-const EventForm = ({ userId, type }: EventFormProps) => {
-  const initialValues = eventDefaultValues;
+const EventForm = ({ userId, type, eventId, event }: EventFormProps) => {
+  const initialValues =
+    event && type === "Update"
+      ? {
+          ...event,
+          startDateTime: new Date(event.startDateTime),
+          endDateTime: new Date(event.startDateTime),
+        }
+      : eventDefaultValues;
+
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues,
@@ -46,6 +57,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
   const { startUpload } = useUploadThing("imageUploader");
 
   const router = useRouter();
+
   async function onSubmit(values: z.infer<typeof eventFormSchema>) {
     const eventData = values;
     let uploadedImageUrl = values.imageUrl;
@@ -69,8 +81,30 @@ const EventForm = ({ userId, type }: EventFormProps) => {
           form.reset();
           router.push(`/events/${newEvent._id}`);
         }
-      } catch (err) {
-        console.log(err);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (type === "Update") {
+      if (!eventId) {
+        router.back();
+        return;
+      }
+
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
+          path: `/events/${eventId}`,
+        });
+
+        if (updatedEvent) {
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
       }
     }
   }
@@ -341,4 +375,5 @@ const EventForm = ({ userId, type }: EventFormProps) => {
     </Form>
   );
 };
+
 export default EventForm;
